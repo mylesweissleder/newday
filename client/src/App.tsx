e5a41e8a0580ac9419fc187824870426e3ea4f1f
mylesweissleder <mylermail@gmail.com>
2025-09-05 14:33:08 -0700

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,44 +1,17 @@
-function App() {
-    // const { user, loading } = useAuth()
+import React from 'react'
+  import { Routes, Route } from 'react-router-dom'
+  import SiteAccessGate from './components/SiteAccessGate'
+  import LoginPage from './pages/LoginPage'
 
-    // if (loading) {
-    //   return (
-    //     <div className="min-h-screen bg-gray-50 flex items-center 
-  justify-center">
-    //       <LoadingSpinner size="lg" />
-    //     </div>
-    //   )
-    // }
-
+  function App() {
     return (
       <SiteAccessGate>
-        {/* {!user ? ( */}
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="*" element={<Navigate to="/login" replace />} />
-          </Routes>
-        {/* ) : (
-          <Layout>
-            <Routes>
-              <Route path="/" element={<Navigate to="/dashboard" replace />}
-   />
-              <Route path="/dashboard" element={<DashboardPage />} />
-              <Route path="/contacts" element={<ContactsPage />} />
-              <Route path="/contacts/:id" element={<ContactDetailPage />} />
-              <Route path="/campaigns" element={<CampaignsPage />} />
-              <Route path="/campaigns/:id" element={<CampaignDetailPage />}
-  />
-              <Route path="/outreach" element={<OutreachPage />} />
-              <Route path="/network" element={<NetworkPage />} />
-              <Route path="/analytics" element={<AnalyticsPage />} />
-              <Route path="/import" element={<ImportPage />} />
-              <Route path="/settings" element={<SettingsPage />} />
-              <Route path="*" element={<Navigate to="/dashboard" replace />}
-   />
-            </Routes>
-          </Layout>
-        )} */}
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="*" element={<LoginPage />} />
+        </Routes>
       </SiteAccessGate>
     )
-}
+  }
+
+  export default App
