01bc1bd3e08a030346d84a96c143b0222a2a7fc1
mylesweissleder <mylermail@gmail.com>
2025-09-05 14:30:54 -0700

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,61 +1,44 @@
-import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import SiteAccessGate from './components/SiteAccessGate'
-import { useAuth } from './hooks/useAuth'
-import Layout from './components/Layout'
-import LoginPage from './pages/LoginPage'
-import RegisterPage from './pages/RegisterPage'
-import DashboardPage from './pages/DashboardPage'
-import ContactsPage from './pages/ContactsPage'
-import ContactDetailPage from './pages/ContactDetailPage'
-import CampaignsPage from './pages/CampaignsPage'
-import CampaignDetailPage from './pages/CampaignDetailPage'
-import OutreachPage from './pages/OutreachPage'
-import NetworkPage from './pages/NetworkPage'
-import AnalyticsPage from './pages/AnalyticsPage'
-import ImportPage from './pages/ImportPage'
-import SettingsPage from './pages/SettingsPage'
-import LoadingSpinner from './components/LoadingSpinner'
-
 function App() {
-  const { user, loading } = useAuth()
+    // const { user, loading } = useAuth()
 
-  if (loading) {
+    // if (loading) {
+    //   return (
+    //     <div className="min-h-screen bg-gray-50 flex items-center 
+  justify-center">
+    //       <LoadingSpinner size="lg" />
+    //     </div>
+    //   )
+    // }
+
     return (
-      <div className="min-h-screen bg-gray-50 flex items-center justify-center">
-        <LoadingSpinner size="lg" />
-      </div>
-    )
-  }
-
-  return (
-    <SiteAccessGate>
-      {!user ? (
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="*" element={<Navigate to="/login" replace />} />
-        </Routes>
-      ) : (
-        <Layout>
+      <SiteAccessGate>
+        {/* {!user ? ( */}
           <Routes>
-            <Route path="/" element={<Navigate to="/dashboard" replace />} />
-            <Route path="/dashboard" element={<DashboardPage />} />
-            <Route path="/contacts" element={<ContactsPage />} />
-            <Route path="/contacts/:id" element={<ContactDetailPage />} />
-            <Route path="/campaigns" element={<CampaignsPage />} />
-            <Route path="/campaigns/:id" element={<CampaignDetailPage />} />
-            <Route path="/outreach" element={<OutreachPage />} />
-            <Route path="/network" element={<NetworkPage />} />
-            <Route path="/analytics" element={<AnalyticsPage />} />
-            <Route path="/import" element={<ImportPage />} />
-            <Route path="/settings" element={<SettingsPage />} />
-            <Route path="*" element={<Navigate to="/dashboard" replace />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="*" element={<Navigate to="/login" replace />} />
           </Routes>
-        </Layout>
-      )}
-    </SiteAccessGate>
-  )
+        {/* ) : (
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Navigate to="/dashboard" replace />}
+   />
+              <Route path="/dashboard" element={<DashboardPage />} />
+              <Route path="/contacts" element={<ContactsPage />} />
+              <Route path="/contacts/:id" element={<ContactDetailPage />} />
+              <Route path="/campaigns" element={<CampaignsPage />} />
+              <Route path="/campaigns/:id" element={<CampaignDetailPage />}
+  />
+              <Route path="/outreach" element={<OutreachPage />} />
+              <Route path="/network" element={<NetworkPage />} />
+              <Route path="/analytics" element={<AnalyticsPage />} />
+              <Route path="/import" element={<ImportPage />} />
+              <Route path="/settings" element={<SettingsPage />} />
+              <Route path="*" element={<Navigate to="/dashboard" replace />}
+   />
+            </Routes>
+          </Layout>
+        )} */}
+      </SiteAccessGate>
+    )
 }
-
-export default App
